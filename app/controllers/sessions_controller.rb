1cef1a61446aa59db04e93024a65b2c1dc3883d7
sachieko <arashikabane@gmail.com>
2022-09-26 02:22:54 -0600

    Login logout functionality, status in nav bar, displays user on order

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by_email(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      p @user['password_digest']
       session[:user_id] = @user.id
       redirect_to '/'
     else
